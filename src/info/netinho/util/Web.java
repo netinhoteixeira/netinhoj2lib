9e7c59ec1f910e838b0f9c5029d8f20932c48293
fco.ernesto@gmail.com <fco.ernesto@gmail.com@db676803-2280-57b8-149b-13904c49d431>
2011-12-24 05:35:56 +0000

    Supports download from a URL.

--- src/info/netinho/util/Web.java
+++ src/info/netinho/util/Web.java
@@ -1,5 +1,15 @@
 package info.netinho.util;
 
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+
 public class Web {
 
     public static int getParameter(String parameter) {
@@ -31,5 +41,67 @@ public class Web {
         }
 
         return retorno;
+    }
+
+    public static String downloadContentStringURL(String url) {
+        URL u;
+        InputStream is = null;
+        DataInputStream dis;
+        StringBuilder sb = new StringBuilder();
+
+        try {
+            u = new URL(url);
+            is = u.openStream();
+            dis = new DataInputStream(new BufferedInputStream(is));
+            while (true) {
+                sb.append((char) dis.readByte());
+            }
+        } catch (EOFException eof) {
+            // Yeah! - Normal program termination
+            //Logger.getLogger(Web.class.getName()).log(Level.SEVERE, null, eof);
+        } catch (MalformedURLException mue) {
+            System.out.println("Ouch - a MalformedURLException happened.");
+            Logger.getLogger(Web.class.getName()).log(Level.SEVERE, null, mue);
+        } catch (IOException ioe) {
+            System.out.println("Oops - an IOException happened.");
+            Logger.getLogger(Web.class.getName()).log(Level.SEVERE, null, ioe);
+        } finally {
+            try {
+                is.close();
+            } catch (IOException ioe) {
+                Logger.getLogger(Web.class.getName()).log(Level.SEVERE, null, ioe);
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static BufferedImage downloadImageURL(String url) {
+        URL u;
+        BufferedInputStream is = null;
+        BufferedImage bi = null;
+
+        try {
+            u = new URL(url);
+            URLConnection hu = (HttpURLConnection) u.openConnection();
+            hu.setRequestProperty("User-agent", "Mozilla/4.0");
+            is = new BufferedInputStream(hu.getInputStream());
+            bi = ImageIO.read(is);
+            is.close();
+        } catch (MalformedURLException mue) {
+            System.out.println("Ouch - a MalformedURLException happened.");
+            Logger.getLogger(Web.class.getName()).log(Level.SEVERE, null, mue);
+        } catch (IOException ioe) {
+            System.out.println("Oops - an IOException happened.");
+            Logger.getLogger(Web.class.getName()).log(Level.SEVERE, null, ioe);
+        } finally {
+            try {
+                is.close();
+            } catch (IOException ioe) {
+                Logger.getLogger(Web.class.getName()).log(Level.SEVERE, null, ioe);
+            }
+        }
+
+        return bi;
     }
 }
